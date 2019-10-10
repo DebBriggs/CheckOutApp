17afb12b021848eb9211f46e4c764e3e27d8a240
DebBriggs <briggsdeborahokorite@gmail.com>
2019-10-10 22:35:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/checkoutapp/browse_products.java
@@ -0,0 +1,14 @@
+package com.example.checkoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class browse_products extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_products);
+    }
+}
---
+++ app/src/main/java/com/example/checkoutapp/products_details.java
@@ -0,0 +1,14 @@
+package com.example.checkoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class products_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products_details);
+    }
+}
